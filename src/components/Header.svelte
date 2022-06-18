8917d8bc4baed5be24f3f311172e084128f8d5b0
artneo7 <contato@artneo.com.br>
2022-06-18 10:11:44 -0300

    check page slug to add active class to menu link

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,10 +1,29 @@
+<script>
+// Check page slug
+import { page } from '$app/stores';
+let slug;
+$: slug = $page.routeId;
+</script>
+
 <header class="header">
   <div class="header__wrapper wrap">
     <a href="/" class="header__logo">NeoMoviesDB</a>
+    <a href="/about" class={`header__link ${slug === 'about' ? ' header__link--active' : ''}`}>About</a>
   </div>
 </header>
 
 <style>
+.header {
+  position: fixed;
+  z-index: 1;
+  width: 100%;
+}
+.header__wrapper {
+  height: 100px;
+  background-color: rgba(0, 0, 0, .1);
+  display: flex;
+  align-items: center;
+}
 .header__logo {
   font-size: 2rem;
   font-weight: bold;
@@ -13,35 +32,37 @@
   padding: .5rem;
   margin-left: -.5rem;
 }
+.header__link {
+  margin-left: auto;
+  padding: 20px;
+  color: #fff;
+  font-weight: bold;
+}
+.header__link--active {
+  text-decoration: underline;
+  text-underline-offset: 4px;
+  text-decoration-thickness: 2px;
+}
 
 /* Add style only to desktop */
 @media(hover: hover) {
   .header {
-    position: fixed;
-    z-index: 1;
-    width: 100%;
     backdrop-filter: blur(8px);
   }
-  .header__wrapper {
-    height: 100px;
-    background-color: rgba(0, 0, 0, .1);
-    display: grid;
-    align-items: center;
+  .header__link:hover {
+    text-decoration: underline;
+    text-underline-offset: 4px;
+    text-decoration-thickness: 2px;
   }
 }
 /* Add style only to mobile */
 @media(hover: none) {
   .header {
-    position: fixed;
-    z-index: 1;
-    width: 100%;
     backdrop-filter: unset;
   }
   .header__wrapper {
     height: 80px;
     background-color: rgba(0, 0, 0, .75);
-    display: grid;
-    align-items: center;
   }
   .header__logo {
     font-size: 1.5rem;
---
+++ src/routes/about.svelte
@@ -0,0 +1,32 @@
+<script>
+import { MetaTags } from "svelte-meta-tags";
+import Title from "../components/Title.svelte";
+</script>
+
+<MetaTags
+  title = "About"
+  titleTemplate="%s | NeoMoviesDB"
+  noindex
+  nofollow
+/>
+
+<main>
+  <section class="intro">
+    <div class="intro__wrapper">
+      <div class="intro__title-wrapper wrap">
+        <Title title={"About"} />
+      </div>
+    </div>
+  </section>
+
+</main>
+
+<style>
+  .intro {
+    height: 460px;
+    padding-top: 142px;
+    background: linear-gradient(rgba(0,0,0,.55),rgba(0,0,0,.55)), url('/images/default-image.jpg');
+    background-position: top;
+    background-size: cover;
+  }
+  </style>
