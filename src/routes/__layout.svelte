7243d8dc3bcd7d27834863efc12f49ec2ec60d01
artneo7 <contato@artneo.com.br>
2022-06-18 09:19:43 -0300

    try to fix favicon issue

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,9 +6,11 @@ import Footer from '../components/Footer.svelte';
 
 <svelte:head>
   <title>NeoMoviesDB</title>
+
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@500;700&display=swap" rel="stylesheet">
+  <link rel="icon" type="image/svg" href="/favicon.png" />
 </svelte:head>
 
 <Header />
